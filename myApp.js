9210dc32b8ebda44cbc34faeffc363c60a36a1c2
duncanjohanne <duncankachasu@gmail.com>
2023-07-28 23:26:31 +0200

    07282326

--- myApp.js
+++ myApp.js
@@ -5,6 +5,8 @@ let app = express();
 
 absPath = __dirname
 
+app.use("/public", express.static(absPath+'/public'))
+
 app.get("/", (req, res) => {
     res.sendFile(absPath+'/views/index.html');
 })
