ccc01be9d41c1c567e680f1db879875ca8868b2f
duncanjohanne <duncankachasu@gmail.com>
2023-07-29 18:15:36 +0200

    07291815

--- myApp.js
+++ myApp.js
@@ -25,6 +25,13 @@ app.get("/json", (req, res) => {
     
 })
 
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({"time":req.time})
+})
+
 
 
 
